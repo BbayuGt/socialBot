ebfc981e07c895df0aa4ac44df90ac29bab28a02
BbayuGt <harusbayu@gmail.com>
2022-06-02 08:13:48 +0700

    initial commit

---
+++ example/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    discord: {
+        token:"",
+        channel:"",
+        server:""
+    },
+    whatsapp: {
+        groupId: "@g.us",
+    }
+}
+
+export default config
--- src/config/config.example.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-    discord: {
-        token:"" // Your Discord token
-    }
-}
-
-export default config
